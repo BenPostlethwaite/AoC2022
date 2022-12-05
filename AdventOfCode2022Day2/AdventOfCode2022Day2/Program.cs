8474ff8037a46335ff0f9cee7ddd5ed0c5c6260a
Ben Postlethwaite <Ben.Postlethwaite05@gmail.com>
2022-12-05 17:38:12 +0000

    test commit

--- AdventOfCode2022Day2/AdventOfCode2022Day2/Program.cs
+++ AdventOfCode2022Day2/AdventOfCode2022Day2/Program.cs
@@ -74,63 +74,62 @@ namespace AoC_day_5
             }
             return line;
 
-
-
-            static void Main(string[] args)
-            {
-
-                string[] data = File.ReadAllLines("test.txt");
-                List<int> xLines = new List<int>();
-                List<int> yLines = new List<int>();
-
-                List<string> graph = new List<string>();
-
-                foreach (string row in data)
-                {
-                    string[] coords = row.Split(' ');
-                    string[] coord1 = coords[0].Split(',');
-                    string[] coord2 = coords[2].Split(',');
-
-
-                    List<int[]> line = new List<int[]>(getLine(coord1, coord2));
-
-                    foreach (int[] point in line)
-                    {
-                        string stringPoint = point[0].ToString() + "," + point[1].ToString();
-                        graph.Add(stringPoint);
-                    }
-
-                }
-
-                int overlaps = 0;
-
-                while (graph.Count > 1)
-                {
-                    string point = graph[0];
-                    bool found = true;
-                    int count = 0;
-
-
-                    int lenBefore = graph.Count;
-                    graph.RemoveAll(item => item == point);
-                    int lenAfter = graph.Count;
-
-                    count = lenBefore - lenAfter;
-
-                    Console.WriteLine(point);
-                    Console.WriteLine(count);
-
-
-                    if (count > 1)
-                    {
-                        overlaps++;
-                    }
-                }
-
-                Console.WriteLine(overlaps);
-
-                Console.ReadKey();
-            }
         }
+
+        static void Main(string[] args)
+        {
+
+            string[] data = File.ReadAllLines("test.txt");
+            List<int> xLines = new List<int>();
+            List<int> yLines = new List<int>();
+
+            List<string> graph = new List<string>();
+
+            foreach (string row in data)
+            {
+                string[] coords = row.Split(' ');
+                string[] coord1 = coords[0].Split(',');
+                string[] coord2 = coords[2].Split(',');
+
+
+                List<int[]> line = new List<int[]>(getLine(coord1, coord2));
+
+                foreach (int[] point in line)
+                {
+                    string stringPoint = point[0].ToString() + "," + point[1].ToString();
+                    graph.Add(stringPoint);
+                }
+
+            }
+
+            int overlaps = 0;
+
+            while (graph.Count > 1)
+            {
+                string point = graph[0];
+                int count = 0;
+
+
+                int lenBefore = graph.Count;
+                graph.RemoveAll(item => item == point);
+                int lenAfter = graph.Count;
+
+                count = lenBefore - lenAfter;
+
+                Console.WriteLine(point);
+                Console.WriteLine(count);
+
+
+                if (count > 1)
+                {
+                    overlaps++;
+                }
+            }
+
+            Console.WriteLine(overlaps);
+
+            Console.ReadKey();
+        }
+        
     }
 }

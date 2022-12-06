3b006c43b8a0ebd97ebfcf3824494776fcf5838f
Ben Postlethwaite <Ben.Postlethwaite05@gmail.com>
2022-12-06 08:14:40 +0000

    day6 halfway

--- AoCDay62022/Program.cs
+++ AoCDay62022/Program.cs
@@ -4,6 +4,26 @@ class Program
     static void Main(string[] args)
     {
         string[] data = File.ReadAllLines("test.txt");
-        
+        foreach (string line in data)
+        {
+            for (int i = 3; i < line.Count(); i++)
+            {
+                string subString = line.Substring(i-3,i);
+                bool marker = true;
+                foreach (char c in subString)
+                {
+                     
+                    if (subString.Count(m => m == c) != 1)
+                    {
+                        marker = false;
+                    }
+                }
+                if (marker == true)
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+            }
+        }
     }
 }

2c4da96e97176d558f209c3fc93f3856d47f6359
BenPostlethwaite <98238741+BenPostlethwaite@users.noreply.github.com>
2022-12-13 09:50:26 +0000

    Day13testing

---
+++ Day13Csharp/Day13Csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day13Csharp
+{
+    internal class Program
+    {
+        static List<string[]> GetData(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            List<string[]> data = new List<string[]>();
+            for (int i = 0; i < lines.Length-2; i+=2)
+            {
+                string[] toComp = {lines[i],lines[i+1]};
+                data.Add(toComp);
+            }
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            List<string[]> data = GetData("test.txt");
+            //foreach (string[] pair in data)
+            //{
+                //string line1 = pair[0];
+                //string line2 = pair[1];
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            //}
+        }
+    }
+}

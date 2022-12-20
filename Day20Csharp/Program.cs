fbc2a433311054bf28d709da8e520b3f825cd9fa
Ben Postlethwaite <Ben.Postlethwaite05@gmail.com>
2022-12-20 20:07:13 +0000

    Day20 Done

--- Day20Csharp/Program.cs
+++ Day20Csharp/Program.cs
@@ -1,64 +1,91 @@
 ﻿using System.Collections.Specialized;
 namespace Day20Csharp;
-class Num : ICloneable
+class Num
 {
-    public int value;
-    public int startIndex;
-    public int currentIndex;
-    public Num(int value, int startIndex, int currentIndex)
+    public long value;
+    public long startIndex;
+    public long currentIndex;
+    public Num(long value, long startIndex, long currentIndex)
     {
         this.value = value;
         this.startIndex = startIndex;
         this.currentIndex = currentIndex;
     }
-    public object Clone()
-    {
-        return this.MemberwiseClone();
-    }
 }
 class Program
 {
-    static int Mod(int num1, int num2)
+    static long Mod(long num1, long num2)
     {
         return (num1%num2 + num2)%num2;
     }
+    static void PrintNums(List<Num> numList)
+    {
+        Console.WriteLine("");
+        foreach (Num num in numList)
+        {
+            Console.Write(((num.value)).ToString());
+            Console.Write(" ");
+        }
+        Console.WriteLine("");
+    }
     static void Main(string[] args)
     {
-        List<int> originalNums = File.ReadAllLines("test.txt")
+        Console.WriteLine("Running...");
+        List<long> originalNums = File.ReadAllLines("data.txt")
         .ToList()
-        .ConvertAll(c => int.Parse(c));
+        .ConvertAll(c => (long)(long.Parse(c)));
 
+        long count = originalNums.Count;
         List<Num> originalNumList = new List<Num>();
         List<Num> numList = new List<Num>();
-        for (int i = 0; i < originalNums.Count; i++)
+        for (long i = 0; i < originalNums.Count; i++)
         {
-            originalNumList.Add(new Num(originalNums[i],i,i));
+            originalNumList.Add(new Num(originalNums[(Int32)i]*(811589153%(count*(count-1))),i,i));
         }
-        int count = originalNumList.Count;
+        
         foreach (Num n in originalNumList)
         {
             numList.Add(n);
         }
 
-
-        foreach (Num numToMove in originalNumList)
+        //PrintNums(numList);
+        for (long round = 0; round < 10; round++)
         {
-            int clicks = Mod(numToMove.value,count);
-            int indexToReach = Mod((numToMove.currentIndex+clicks),(count));
-            //Num originalNum = originalNumList[i];
-            if (clicks != 0)
+            foreach (Num numToMove in originalNumList)
             {
-                int i = numToMove.currentIndex;
-                while (i != indexToReach)
+                long clicks = Mod(numToMove.value,count-1);
+                long indexToReach = Mod((numToMove.currentIndex+clicks),(count));
+                if (clicks != 0)
                 {
-                    numList[i] = numList[Mod((i+1),count)];
-                    numList[i].currentIndex = i;
-                    i++;
-                    i = Mod(i,count);
+                    long i = numToMove.currentIndex;
+                    while (i != indexToReach)
+                    {
+                        numList[(Int32)i] = numList[(Int32)Mod((i+1),count)];
+                        numList[(Int32)i].currentIndex = i;
+                        i++;
+                        i = Mod(i,count);
+                    }
+                    numToMove.currentIndex = indexToReach;
+                    numList[(Int32)numToMove.currentIndex] = numToMove;
                 }
-                numToMove.currentIndex = indexToReach;
-                numList[numToMove.currentIndex] = numToMove;
+                //PrintNums(numList);
             }
+            //PrintNums(numList);
         }
+
+
+
+        long indexOfZero = originalNumList[originalNums.IndexOf(0)].currentIndex;
+        List<long> toSum = new List<long>();
+        for (long i = 1000; i <= 3000; i+= 1000)
+        {
+            toSum.Add((long)numList[(Int32)(indexOfZero+i)%(Int32)(count)].value/((long)811589153%(count*(count-1)))*811589153);
+        }
+        long total = 0;
+        foreach (long num in toSum)
+        {
+            total += num;
+        }
+        Console.WriteLine(total);
     }
 }
